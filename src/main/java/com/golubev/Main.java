5e3bdd58e110449b0248bf78732dc50edcd09e89
Golubev <golubev.s.g.25@gmail.com>
2022-04-07 14:00:44 +0300

    changed 2

--- src/main/java/com/golubev/Main.java
+++ src/main/java/com/golubev/Main.java
@@ -3,5 +3,7 @@ package com.golubev;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Ура мы подключили GitHub Actions");
+        var1 := 1;
     }
 }
